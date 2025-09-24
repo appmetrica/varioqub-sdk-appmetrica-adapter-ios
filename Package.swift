d0095d609bb446c03d11808ef0d98f7b5bd8580b
alex-oleynik <alex-oleynik@yandex-team.com>
2025-09-24 08:11:43 +0300

    Varioqub iOS SDK version update 1.1.1 commit_hash:0d452a1d67e795e2d1db4ab15f8600abe3634122

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let usedSource: DependencySource = .regular
-let varioqubCurrentVersion = "1.1.0"
+let varioqubCurrentVersion = "1.1.1"
 let spmExternalScope = "spm-external"
 
 let swiftCompilerSettings: [SwiftSetting] = [
